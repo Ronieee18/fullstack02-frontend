ebe85cfd76b74aae0c197c31792e83e8e8cfc947
Ronieee18 <ronitparwani78@gmail.com>
2024-02-11 14:07:59 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://fullstack02-backend.onrender.com',
+      '/api':'https://fullstack02-backend.onrender.com/',
     }
   },
   plugins: [react()],
